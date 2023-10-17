a98ba6a4229a82bfcd1464117b965108b3a9bb69
westlifeswift <2295857249@qq.com>
2023-10-17 21:56:50 +0800

    delete

--- packages/project2/__tests__/project2.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const project2 = require('..');
-
-describe('project2', () => {
-    it('needs tests');
-});
--- packages/project2/lib/project2.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = project2;
-
-function project2() {
-    // TODO
-}
